c5dbd31cf31ae2c64489046b9230a757f9b40089
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-10-28 09:19:42 -0700

    commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ server.get('/hobbits', (req, res) => {
     },
   ]
   res.status(200).json(hobbits)
-})
+});
 
 server.get('/users', (req, res) => {
   users.find()
@@ -30,8 +30,8 @@ server.get('/users', (req, res) => {
   .catch((error) => {
     res.status(500).json({message: "unable to get users"})
   })
-})
+});
 
 server.listen(8000, () => {
   console.log('API running on port 8000')
-})
+});
