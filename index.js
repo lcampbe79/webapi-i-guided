95ba14c624abf0632a568d91169e59758db96f6b
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-10-28 12:22:48 -0700

    Pushing after lecture.

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const users = require('./data/hubs-model')
 
 const server= express();
 
+//middleware
+server.use(express.json());
+
 server.get('/', (req, res) => {
   res.send('Hello World');
 });
@@ -21,6 +24,43 @@ server.get('/hobbits', (req, res) => {
   ]
   res.status(200).json(hobbits)
 });
+
+//same as getting users
+server.get('/hubs', (req, res) => {
+  users.find()
+  .then(hubs => {
+    res.json(hubs)
+  })
+  .catch(err => {
+    res.json({message: "Failed to get hubs from DB"})
+  })
+})
+
+server.post('/hubs', (req, res) => {
+  const hubInfo = req.body;
+
+  console.log('hub info', hubInfo)
+
+  users.add(hubInfo)
+  .then(hub => {
+    res.status(201).json(hub)
+  })
+  .catch(err => {
+    res.status(500).json({message: "Failed to add hubs from DB"})
+  })
+})
+
+server.delete('/hubs/:id', (req, res) => {
+  const id = req.params.id;
+
+  users.remove(id)
+  .then(count => {
+    res.status(200).json({message: `hubs with id ${id} deleted`})
+  })
+  .catch(err => {
+    res.status(500).json({message: "Failed to delete hubs from DB"})
+  })
+})
 
 server.get('/users', (req, res) => {
   users.find()
